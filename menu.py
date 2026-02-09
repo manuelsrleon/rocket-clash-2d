05ab1c3f5f298d90558594f40a54e76ddded01a4
manuelsrleon <manuelsrleon@gmail.com>
2026-02-09 23:52:09 +0100

    menu fixes

--- menu.py
+++ menu.py
@@ -17,29 +17,71 @@ class GUIElement:
         raise NotImplemented("Action method not implemented")
 
 class Button(GUIElement):
+    
     def __init__(self, screen, imageName, position):
         self.image = None # add img loading logic
-        self.image = pygame.transform.scale(self.image, (20,20))
-        GUIElement.__init__(self, screen, self.image.get_rect())
+        #self.image = pygame.transform.scale(self.image, (20,20))
+        #GUIElement.__init__(self, screen, self.image.get_rect())
         self.setPosition(position)
+    
+    def render(self, screen):
+        screen.blit(self.image, self.rect)
 
 class PlayButton(Button):
+    
     def __init__(self,screen):
         Button.__init__(self, screen, 'play_button.png', (580, 530))
+    
     def action(self):
         self.screen.menu.playCampaign()
 
 class ExitButton(Button):
+    
     def __init__(self, screen):
         Button.__init__(self, screen, 'exit_button.png', (580, 560))
+    
     def action(self):
         self.screen.menu.exit()
 
+class GUIScreen:
+    def __init(self,menu,imageName):
+        self.menu = menu
+        self.image = None #TODO Load image
+        self.image = pygame.transform.scale(self.image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.GUIElements = []
+        self.animations = []
+
+    def events(self, event_list):
+        for event in event_list:
+            if event.type == MOUSEBUTTONDOWN:
+                self.clickElement = None
+                for GUIElement in self.GUIElements:
+                    if element.positionInElement(event.position):
+                        self.elementClick = element
+            if event.type == MOUSEBUTTONUP:
+                for element in self.GUIElements:
+                    if element.positionInElement(event.pos):
+                        if(element == self.elementClick):
+                            element.action()
+    
+    def render(self, screen):
+        screen.blit(self.image, self.image.get_rect())
+        for element in self.GUIElements:
+            element.render(screen)
+
+class InitialGUIScreen(GUIScreen):
+    def __init__(self, menu):
+        GUIScreen.__init__(self)
+        playButton = PlayButton(self)
+        exitButton = ExitButton(self)
+        self.GUIElements.append(playButton)
+        self.GUIElements.append(exitButton)
+
 class Menu(PyGameScene):
-    def __init(self, director):
+    def __init__(self, director):
         PyGameScene.__init__(self,director)
         self.screenList = []
-        self.screenList.append(InitialGUISscreen(self))
+        self.screenList.append(InitialGUIScreen(self))
         self.showInitialScreen
     def update(self, *args):
         return
