90a90c7f10fef6cd5cdcb354a1b65a95837b0d0e
manuelsrleon <manuelsrleon@gmail.com>
2026-03-15 19:21:03 +0100

    uncomment scenes

--- menu.py
+++ menu.py
@@ -125,11 +125,11 @@ class Menu(PyGameScene):
 
         d = self.director
         sequence = [
-            #lambda: DialogueScene(d, "dialogues/intro.json"),
-            #lambda: DialogueScene(d, "dialogues/match1.json"),
-            #lambda: FirstScene(d),
-            #lambda: DialogueScene(d, "dialogues/match1_end.json"),
-            #lambda: DialogueScene(d, "dialogues/match2.json"),
+            lambda: DialogueScene(d, "dialogues/intro.json"),
+            lambda: DialogueScene(d, "dialogues/match1.json"),
+            lambda: FirstScene(d),
+            lambda: DialogueScene(d, "dialogues/match1_end.json"),
+            lambda: DialogueScene(d, "dialogues/match2.json"),
             lambda: SecondScene(d),
             lambda: DialogueScene(d, "dialogues/match2_end.json"),
             lambda: DialogueScene(d, "dialogues/match3.json"),
